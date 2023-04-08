492b1934ddf4126ea2fd528f79b776642f2f7c47
Franzzzzzzzz <francois-guill.guillard2@laposte.net>
2023-04-08 17:46:20 +1000

    Moving to a cmake compiling setup

--- Test.cpp
+++ Test.cpp
@@ -1,18 +1,20 @@
 #include <cstdlib>
 #include <cstdio>
-#include <map>
+#include <iostream>
 
-using namespace std ; 
+#include <libintl.h>
+#define _(String) gettext (String)
+#define gettext_noop(String) String
+#define N_(String) gettext_noop (String)
 
 int main ()
 {
- map <int, int> hello ; 
-
-
- for (int i=10 ; i>0 ; i--)
-   hello[i]+=i ; 
- 
- for (int i=1 ; i<5 ; i++)
-   printf("%d %d \n",i, hello[i]) ;  
+ setlocale (LC_ALL, "");
+ bindtextdomain ("hello", "/usr/share/locale/");
+ textdomain ("hello");
+  
+  
+ printf(_("Ceci est un test. %d %g\n"), 8, 3.14159) ; 
+ std::cout << _("Un autre test ") << 3.145 << "\n" ; 
  
 }
